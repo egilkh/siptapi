516c5914f018873b2c16c8438748dbb54a7b14c4
klaus_darilion <klaus_darilion@16e85aaa-c415-4400-99f9-a0709d1d1986>
2010-12-14 15:28:19 +0000

    - updated version string to 0.2.10 - workaround for xlite in alternative configuration

--- WaveTsp.h
+++ WaveTsp.h
@@ -30,11 +30,11 @@
 #define DIM(rg) (sizeof(rg)/sizeof(*rg))
 
 #ifdef _DEBUG
-#define SIPTAPI_VERSION_W L"v0.2.9 Debug"
-#define SIPTAPI_VERSION    "v0.2.9 Debug"
+#define SIPTAPI_VERSION_W L"v0.2.10 Debug"
+#define SIPTAPI_VERSION    "v0.2.10 Debug"
 #else
-#define SIPTAPI_VERSION_W L"v0.2.9 Release"
-#define SIPTAPI_VERSION    "v0.2.9 Release"
+#define SIPTAPI_VERSION_W L"v0.2.10 Release"
+#define SIPTAPI_VERSION    "v0.2.10 Release"
 #endif
 
 // Data sent back and forth between UI and TSP during a dial operation
